8e70e258409813de5e227a2824a632c8cf88ef5b
Denise <denise.vecino@gmail.com>
2024-03-13 18:23:33 -0300

    Adapt to the new requirements and change to use ADWIN detector from River library.

--- run_thesis_experiments_massive.py
+++ run_thesis_experiments_massive.py
@@ -26,14 +26,14 @@ class Dataset1Configuration:
     ###############################################################
     # Information about the data for performing the experiments
     ###############################################################
-    input_path = 'C:/Users/denis/OneDrive/Documents/Doutorado/Bases de Dados/DadosConceptDrift/IPDD_Datasets/dataset1'
+    input_path = 'datasets/dataset1'
     lognames2500 = [
         'cb2.5k.xes',
         'cd2.5k.xes',
         'cf2.5k.xes',
         'cm2.5k.xes',
         'cp2.5k.xes',
-        # 'fr2.5k.xes',
+        # 'fr2.5k.xes', # not used because IPDD only detects structural drifts in the control-flow perspective
         'IOR2.5k.xes',
         'IRO2.5k.xes',
         'lp2.5k.xes',
@@ -53,7 +53,7 @@ class Dataset1Configuration:
         'cf5k.xes',
         'cm5k.xes',
         'cp5k.xes',
-        # 'fr5k.xes',
+        # 'fr5k.xes', # not used because IPDD only detects structural drifts in the control-flow perspective
         'IOR5k.xes',
         'IRO5k.xes',
         'lp5k.xes',
@@ -73,7 +73,7 @@ class Dataset1Configuration:
         'cf7.5k.xes',
         'cm7.5k.xes',
         'cp7.5k.xes',
-        # 'fr7.5k.xes',
+        # 'fr7.5k.xes', # not used because IPDD only detects structural drifts in the control-flow perspective
         'IOR7.5k.xes',
         'IRO7.5k.xes',
         'lp7.5k.xes',
@@ -93,7 +93,7 @@ class Dataset1Configuration:
         'cf10k.xes',
         'cm10k.xes',
         'cp10k.xes',
-        # 'fr10k.xes',
+        # 'fr10k.xes', # not used because IPDD only detects structural drifts in the control-flow perspective
         'IOR10k.xes',
         'IRO10k.xes',
         'lp10k.xes',
@@ -112,11 +112,6 @@ class Dataset1Configuration:
     windows = [i for i in range(25, 301, 25)]
     deltas = [0.002, 0.05, 0.1, 0.3]
 
-    # for testing one specific scenario
-    lognames = ['cb2.5k.xes', 'cd5k.xes']
-    windows = [75, 100]
-    deltas = [0.002]
-
     ###############################################################
     # Information for calculating evaluation metrics
     ###############################################################
@@ -140,49 +135,112 @@ class Dataset1Configuration:
     # For defining the correct order for the legend of the plots
     order_legend = [1, 2, 3, 0]
 
-class Dataset1Configuration_Sample:
+
+class Dataset2Configuration:
     ###############################################################
     # Information about the data for performing the experiments
     ###############################################################
-    input_path = 'C:/Users/denis/OneDrive/Documents/Doutorado/Bases de Dados/DadosConceptDrift/IPDD_Datasets/dataset1'
-    lognames = [
-        'cb2.5k.xes',
-        'cb7.5k.xes',
-        'cb5k.xes',
-        'cb10k.xes',
+    input_path = 'datasets/dataset2'
+    lognames3000 = [
+        'cb3k.xes',
+        'cd3k.xes',
+        'cf3k.xes',
+        'cm3k.xes',
+        'cp3k.xes',
+        'IOR3k.xes',
+        'IRO3k.xes',
+        'lp3k.xes',
+        'OIR3k.xes',
+        'ORI3k.xes',
+        'pl3k.xes',
+        'pm3k.xes',
+        're3k.xes',
+        'RIO3k.xes',
+        'ROI3k.xes',
+        'rp3k.xes',
+        'sw3k.xes',
+    ]
+    lognames4500 = [
+        'cb4.5k.xes',
+        'cd4.5k.xes',
+        'cf4.5k.xes',
+        'cm4.5k.xes',
+        'cp4.5k.xes',
+        'IOR4.5k.xes',
+        'IRO4.5k.xes',
+        'lp4.5k.xes',
+        'OIR4.5k.xes',
+        'ORI4.5k.xes',
+        'pl4.5k.xes',
+        'pm4.5k.xes',
+        're4.5k.xes',
+        'RIO4.5k.xes',
+        'ROI4.5k.xes',
+        'rp4.5k.xes',
+        'sw4.5k.xes',
+    ]
+    lognames8000 = [
+        'cb8k.xes',
+        'cd8k.xes',
+        'cf8k.xes',
+        'cm8k.xes',
+        'cp8k.xes',
+        'IOR8k.xes',
+        'IRO8k.xes',
+        'lp8k.xes',
+        'OIR8k.xes',
+        'ORI8k.xes',
+        'pl8k.xes',
+        'pm8k.xes',
+        're8k.xes',
+        'RIO8k.xes',
+        'ROI8k.xes',
+        'rp8k.xes',
+        'sw8k.xes',
     ]
 
-    # for testing one specific scenario
-    windows = [75]
-    deltas = [0.5]
+    lognames = lognames3000 + lognames4500 + lognames8000
+    windows = [i for i in range(25, 301, 25)]
+    deltas = [0.002, 0.05, 0.1, 0.3]
 
     ###############################################################
     # Information for calculating evaluation metrics
     ###############################################################
     actual_change_points = {
-        '2.5k': define_change_points_dataset1(250),
-        '5k': define_change_points_dataset1(500),
-        '7.5k': define_change_points_dataset1(750),
-        '10k': define_change_points_dataset1(1000)
+        '3k': [],
+        '4.5k': [],
+        '8k': [],
     }
 
     number_of_instances = {
-        '2.5k': 2500,
-        '5k': 5000,
-        '7.5k': 7500,
-        '10k': 10000
+        '3k': 3000,
+        '4.5k': 4500,
+        '8k': 8000,
     }
 
+
+class RealDatasetConfiguration:
     ###############################################################
-    # Plot specific information
+    # Information about the data for performing the experiments
     ###############################################################
-    # For defining the correct order for the legend of the plots
-    order_legend = [1, 2, 3, 0]
+    input_path = 'datasets/real'
+    lognames = ['italian_help_desk_company.xes']
+    windows = [100]
+    deltas = [0.002]
 
 
 if __name__ == '__main__':
     dataset1 = Dataset1Configuration()
-    run_massive_fixed_controlflow(dataset1)
+    # run_massive_fixed_controlflow(dataset1)
     run_massive_adaptive_controlflow_trace_by_trace(dataset1, evaluate=True)
     run_massive_adaptive_controlflow_windowing(dataset1, evaluate=True)
 
+    dataset2 = Dataset2Configuration()
+    # run_massive_fixed_controlflow(dataset2)
+    run_massive_adaptive_controlflow_trace_by_trace(dataset2, evaluate=True)
+    run_massive_adaptive_controlflow_windowing(dataset2, evaluate=True)
+
+    real_dataset = RealDatasetConfiguration()
+    # run_massive_fixed_controlflow(real_dataset)
+    run_massive_adaptive_controlflow_trace_by_trace(real_dataset)
+    run_massive_adaptive_controlflow_windowing(real_dataset)
