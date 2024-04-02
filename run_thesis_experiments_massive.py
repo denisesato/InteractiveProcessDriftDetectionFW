73fdb8049cf383de2bcd2a02924e4223620adb56
Denise <denise.vecino@gmail.com>
2024-04-02 18:38:38 -0300

    - Include results from other tools experiments - Include new results from IPDD after updating libraries - Include and adapt analyze_results file for generating plots and statistical analysis using the new results. The adaptation consists on considering new format of the spreadsheet with the massive results after including the new detector HDDMW.

--- run_thesis_experiments_massive.py
+++ run_thesis_experiments_massive.py
@@ -255,15 +255,15 @@ class RealDatasetConfiguration:
 
 
 if __name__ == '__main__':
-    dataset1 = Dataset1Configuration()
-    # run_massive_fixed_controlflow(dataset1)
-    run_massive_adaptive_controlflow_trace_by_trace(dataset1, evaluate=True)
-    run_massive_adaptive_controlflow_windowing(dataset1, evaluate=True)
-
-    dataset2 = Dataset2Configuration()
-    # run_massive_fixed_controlflow(dataset2)
-    run_massive_adaptive_controlflow_trace_by_trace(dataset2, evaluate=True)
-    run_massive_adaptive_controlflow_windowing(dataset2, evaluate=True)
+    # dataset1 = Dataset1Configuration()
+    # # run_massive_fixed_controlflow(dataset1)
+    # run_massive_adaptive_controlflow_trace_by_trace(dataset1, evaluate=True)
+    # run_massive_adaptive_controlflow_windowing(dataset1, evaluate=True)
+    #
+    # dataset2 = Dataset2Configuration()
+    # # run_massive_fixed_controlflow(dataset2)
+    # run_massive_adaptive_controlflow_trace_by_trace(dataset2, evaluate=True)
+    # run_massive_adaptive_controlflow_windowing(dataset2, evaluate=True)
 
     real_dataset = RealDatasetConfiguration()
     # run_massive_fixed_controlflow(real_dataset)
