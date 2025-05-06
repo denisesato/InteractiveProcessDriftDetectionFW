0d6e1bb278d9e47a88844f20ca588f8cfb0aa157
denisevecino <denise.vecino@gmail.com>
2025-05-05 22:57:40 -0300

    configuration of Sheila's experiments.

--- run_scenarios_Sheila.py
+++ run_scenarios_Sheila.py
@@ -33,7 +33,8 @@ class Dataset1Configuration:
     ###############################################################
     # Information about the data for performing the experiments
     ###############################################################
-    input_path = 'C:/Users/Denise/OneDrive/Documents/Doutorado/Bases de Dados/DadosConceptDrift/IPDD_Datasets/dataset1'
+    dataset_name = 'dataset1'
+    input_path = 'datasets/dataset1'
     lognames2500 = [
         'cb2.5k.xes',
         'cd2.5k.xes',
@@ -118,11 +119,13 @@ class Dataset1Configuration:
     lognames = lognames2500 + lognames5000 + lognames7500 + lognames10000
 
     # for testing one specific scenario
-    lognames = ['cb2.5k.xes', 'pm2.5k.xes', 're2.5k.xes']
-    # windows = [i for i in range(25, 301, 25)]
-    windows = [75, 100]
+    # lognames = ['cb2.5k.xes', 'pm2.5k.xes', 're2.5k.xes']
+    windows = [i for i in range(25, 301, 25)]
     detectors = [
-        SelectDetector.get_detector_instance(ConceptDriftDetector.ADWIN.name),
+        SelectDetector.get_detector_instance(ConceptDriftDetector.ADWIN.name, parameters={'delta': 0.002}),
+        SelectDetector.get_detector_instance(ConceptDriftDetector.ADWIN.name, parameters={'delta': 0.05}),
+        SelectDetector.get_detector_instance(ConceptDriftDetector.ADWIN.name, parameters={'delta': 0.1}),
+        SelectDetector.get_detector_instance(ConceptDriftDetector.ADWIN.name, parameters={'delta': 0.3}),
         SelectDetector.get_detector_instance(ConceptDriftDetector.HDDM_W.name,
                                              parameters={'two_sided_test': True})
     ]
@@ -150,6 +153,96 @@ class Dataset1Configuration:
     # For defining the correct order for the legend of the plots_thesis
     order_legend = [1, 2, 3, 0]
 
+class Dataset2Configuration:
+    ###############################################################
+    # Information about the data for performing the experiments
+    ###############################################################
+    dataset_name = 'dataset2'
+    input_path = 'datasets/dataset2'
+    lognames3000 = [
+        'cb3k.xes',
+        'cd3k.xes',
+        'cf3k.xes',
+        'cm3k.xes',
+        'cp3k.xes',
+        'IOR3k.xes',
+        'IRO3k.xes',
+        'lp3k.xes',
+        'OIR3k.xes',
+        'ORI3k.xes',
+        'pl3k.xes',
+        'pm3k.xes',
+        're3k.xes',
+        'RIO3k.xes',
+        'ROI3k.xes',
+        'rp3k.xes',
+        'sw3k.xes',
+    ]
+    lognames4500 = [
+        'cb4.5k.xes',
+        'cd4.5k.xes',
+        'cf4.5k.xes',
+        'cm4.5k.xes',
+        'cp4.5k.xes',
+        'IOR4.5k.xes',
+        'IRO4.5k.xes',
+        'lp4.5k.xes',
+        'OIR4.5k.xes',
+        'ORI4.5k.xes',
+        'pl4.5k.xes',
+        'pm4.5k.xes',
+        're4.5k.xes',
+        'RIO4.5k.xes',
+        'ROI4.5k.xes',
+        'rp4.5k.xes',
+        'sw4.5k.xes',
+    ]
+    lognames8000 = [
+        'cb8k.xes',
+        'cd8k.xes',
+        'cf8k.xes',
+        'cm8k.xes',
+        'cp8k.xes',
+        'IOR8k.xes',
+        'IRO8k.xes',
+        'lp8k.xes',
+        'OIR8k.xes',
+        'ORI8k.xes',
+        'pl8k.xes',
+        'pm8k.xes',
+        're8k.xes',
+        'RIO8k.xes',
+        'ROI8k.xes',
+        'rp8k.xes',
+        'sw8k.xes',
+    ]
+
+    lognames = lognames3000 + lognames4500 + lognames8000
+    windows = [i for i in range(25, 301, 25)]
+    detectors = [
+        SelectDetector.get_detector_instance(ConceptDriftDetector.ADWIN.name, parameters={'delta': 0.002}),
+        SelectDetector.get_detector_instance(ConceptDriftDetector.ADWIN.name, parameters={'delta': 0.05}),
+        SelectDetector.get_detector_instance(ConceptDriftDetector.ADWIN.name, parameters={'delta': 0.1}),
+        SelectDetector.get_detector_instance(ConceptDriftDetector.ADWIN.name, parameters={'delta': 0.3}),
+        SelectDetector.get_detector_instance(ConceptDriftDetector.HDDM_W.name,
+                                             parameters={'two_sided_test': True})
+    ]
+
+
+    ###############################################################
+    # Information for calculating evaluation metrics
+    ###############################################################
+    actual_change_points = {
+        '3k': [250, 750, 1500, 2500],
+        '4.5k': [250, 750, 1500, 2500, 3250, 3750, 4000],
+        '8k': [250, 750, 1500, 2500, 3250, 3750, 4000, 4500, 5250, 6250, 7000, 7500, 7750],
+    }
+
+    number_of_instances = {
+        '3k': 3000,
+        '4.5k': 4500,
+        '8k': 8000,
+    }
 
 def run_adaptive_control_flow():
     input_path = 'datasets/dataset1'
@@ -180,7 +273,10 @@ def run_adaptive_control_flow():
 
 
 if __name__ == '__main__':
-    run_adaptive_control_flow()
-    # dataset1 = Dataset1Configuration()
-    # run_massive_adaptive_controlflow_trace_by_trace(dataset1, evaluate=True, save_sublogs=True)
-    # run_massive_adaptive_controlflow_windowing(dataset1, evaluate=True, save_sublogs=True)
+    # run_adaptive_control_flow()
+    dataset1 = Dataset1Configuration()
+    run_massive_adaptive_controlflow_trace_by_trace(dataset1, evaluate=True, save_sublogs=True, save_model_svg=True)
+    run_massive_adaptive_controlflow_windowing(dataset1, evaluate=True, save_sublogs=True, save_model_svg=True)
+    dataset2= Dataset2Configuration()
+    run_massive_adaptive_controlflow_trace_by_trace(dataset2, evaluate=True, save_sublogs=True, save_model_svg=True)
+    run_massive_adaptive_controlflow_windowing(dataset2, evaluate=True, save_sublogs=True, save_model_svg=True)
